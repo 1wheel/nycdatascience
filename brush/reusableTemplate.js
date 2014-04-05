67c340fb098f9d5bd24ec7f533ca443e72e62a2f
Vivian Zhang <vivian.stanford@gmail.com>
2014-04-05 14:29:44 -0400

    submit all changes for week 4 class"

--- brush/reusableTemplate.js
+++ brush/reusableTemplate.js
@@ -4,6 +4,7 @@ graphs.scatter = function(){
       data,
       parentEl,
       currentHover,
+      brush,
       dispatch = d3.dispatch('hover');
 
   var graph = {};
@@ -11,8 +12,32 @@ graphs.scatter = function(){
     d3.select(parentEl).select('svg').remove();
     
     svg = d3.select(parentEl).append('svg')
+        .attr({x: 400, y: 400});
 
-    //.on('mouseover', function(d, i){ dispatch.hover(d) })
+    var x = d3.scale.linear()
+        .domain(d3.extent(data.map(function(d) { return d.date; })))
+        .range([0, 400])
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data.map(function(d) { return d.price; }))])
+        .range([400, 0])
+
+    var maxExtent = x.domain();
+
+
+    svg.selectAll('circle')
+        .data(data).enter()
+      .append('circle')
+        .attr('cx', function(d){ return x(d.date); })
+        .attr('cy', function(d){ return y(d.price); })
+        .attr('r', 5)
+        .on('mouseover', function(d, i){ dispatch.hover(d) })
+
+    brush.on('brush', function(){
+      x.domain(brush.empty() ? maxExtent : brush.extent());
+      svg.selectAll('circle')
+          .attr('cx', function(d){ return x(d.date); })
+          .attr('cy', function(d){ return y(d.price); })
+    })
 
     return graph;
   }
@@ -26,6 +51,12 @@ graphs.scatter = function(){
   graph.parentEl = function(_x){
     if (!arguments.length) return parentEl;
     parentEl = _x;
+    return graph;
+  }
+
+  graph.brush = function(_x){
+    if (!arguments.length) return brush;
+    brush = _x;
     return graph;
   }
 
